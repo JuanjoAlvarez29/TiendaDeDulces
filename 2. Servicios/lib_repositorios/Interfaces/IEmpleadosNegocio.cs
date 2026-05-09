daee92945f94c802d911e95e5c3da002cfbf4f7a
Juanjo Alvarez <juanjoalvarezrios@gmail.com>
2026-05-09 00:01:28 -0500

    Cambio de interfaces e Implementaciones

---
+++ 2. Servicios/lib_repositorios/Interfaces/IEmpleadosNegocio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aplicaciones_libreria.Interfaces
+{
+    internal class IEmpleadosNegocio
+    {
+    }
+}
---
+++ 2. Servicios/lib_repositorios/Interfaces/IEntregasNegocio.cs
@@ -0,0 +1,13 @@
+﻿
+using Libreria_repositorios.Entidades;
+
+namespace Aplicaciones_libreria.Interfaces
+{
+    public interface IEntregasNegocio
+    {
+        List<Entregas> Consultar();
+        Entregas Guardar(Entregas entidad);
+        Entregas Modificar(Entregas entidad);
+        bool Borrar(int id);
+    }
+}
